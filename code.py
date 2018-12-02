ad4f2cfa50e728b5bb2565d4a45e3f2d368ce9d2
Taimoor Chatoor <tchatoor@gmail.com>
2018-12-02 14:18:14 -0500

    Update code.py

--- code.py
+++ code.py
@@ -67,3 +67,10 @@ movie.rename(columns={'id':'movie_id'},inplace=True)
 dataset=pd.merge(movie,mc,on='movie_id')
 dataset = dataset[['title', 'movie_id', 'director', 'cast', 'genres','overview','keywords','tagline','homepage','original_language','production_companies','production_countries','release_date','runtime','popularity']]
 dataset = dataset[pd.notnull(dataset['overview'])]
+
+dataset = dataset[['title', 'movie_id', 'director', 'cast', 'genres','overview','keywords','tagline','homepage','original_language','production_companies','production_countries','release_date','runtime','popularity']]
+dataset
+dataset['overview'].head()
+dataset['overview'].isna().sum()
+dataset = dataset[pd.notnull(dataset['overview'])]
+dataset['overview']
