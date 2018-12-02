e426c7ec1971ffbd8e28d1bb9497afd79952cd08
wr2313 <44385262+wr2313@users.noreply.github.com>
2018-12-02 15:00:31 -0500

    add netinfo function

--- Functions/code.py
+++ Functions/code.py
@@ -68,6 +68,16 @@ dataset=pd.merge(movie,mc,on='movie_id')
 dataset = dataset[['title', 'movie_id', 'director', 'cast', 'genres','overview','keywords','tagline','homepage','original_language','production_companies','production_countries','release_date','runtime','popularity']]
 dataset = dataset[pd.notnull(dataset['overview'])]
 
-dataset['overview'].head()
-dataset['overview'].isna().sum()
-dataset['overview']
+
+
+
+#function used to return basic info of the movie to the user
+def getinfo(x):
+    for i in range(len(dataset)):
+        if dataset.iloc[i]['title']==x:
+            print('\nDetailed Info: \n')
+            print('Title: ' +x +"\nGenres: "
+                  +str(dataset.iloc[i]['genres']).strip('[]').replace(' ','')
+                  +"\nHomepage: "
+                  +str(dataset.iloc[i]['homepage']).strip('[]').replace(' ','')
+                  +'\nOverview:\n' +str(dataset.iloc[i]['overview'])) 
